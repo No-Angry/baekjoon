aced645f56d67cddc472f6fb3562f5fd2afafe97
SeongHyun Kwon <ashyeon0422@gmail.com>
2025-01-03 10:52:32 +0900

    [Silver III] Title: 소수 구하기, Time: 1860 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+int check_sosu(unsigned int s) {
+    if (s == 1) return 0; // 1은 소수가 아님
+    else if (s == 2 || s == 3) return 1; // 2, 3은 소수
+    else if (s % 2 == 0 || s % 3 == 0) return 0; // 2와 3으로 나누어 떨어지면 소수 아님.
+    for (int i = 5; i * i <= s; i += 6) { // 소수는 4 이상이면 6k - 1 혹은 6k + 1을 판별. 조건에 만족하면 소수
+        if (s % i == 0 || s % (i + 2) == 0) return 0;
+    }
+    return 1;
+}
+
+int main(void) {
+    int start, end;
+    cin >> start >> end; // 소수 판별할 기준 지정
+
+    while (start <= end) {
+        if (check_sosu(start)) cout << start << endl;
+        start++;
+    }
+    return 0;
+}
