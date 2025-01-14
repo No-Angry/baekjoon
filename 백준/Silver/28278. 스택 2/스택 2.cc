2c0b320eaeb3bf4884a92374cd15bfd9e3b1b3a0
SeongHyun Kwon <ashyeon0422@gmail.com>
2025-01-14 11:17:38 +0900

    [Silver IV] Title: 스택 2, Time: 140 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.cc
+++ 백준/Silver/28278. 스택 2/스택 2.cc
@@ -1,42 +1,63 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+struct stack {
+	int* arr;
+	int last;
 
+	void init(int n) {
+		arr = new int[n];
+		last = -1;
+	}
+	~stack() {
+		delete[] arr;
+	}
+	void push(int data) {
+		arr[++last] = data;
+	}
+	int pop(void) {
+		if (empty()) return -1;
+		return arr[last--];
+	}
+	int size(void) {
+		return last + 1;
+	}
+	bool empty(void) {
+		return (last < 0);
+	}
+	int top(void) {
+		if (empty()) return -1;
+		return arr[last];
+	}
+
+};
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	vector<int> arr;
-	int input;
-	cin >> input;
-	for (int i = 0; i < input; i++) {
-		int proc;
+	int n;
+	cin >> n;
+	stack s;
+	s.init(n);
+	for (; n--;) { // n--를 수행하고 0이면 종료
+		int proc, x;
 		cin >> proc;
-		if (proc == 1) {
-			int x;
+		switch (proc) {
+		case 1:
 			cin >> x;
-			arr.push_back(x);
-		}
-		else if (proc == 2){
-			if (arr.size() == 0) cout << -1 << '\n';
-			else {
-				cout << arr[arr.size() - 1] << '\n';
-				arr.pop_back();
-			}
-		}
-		else if (proc == 3) {
-			cout << arr.size() << '\n';
-		}
-
-		else if (proc == 4) {
-			if (arr.size() == 0) cout << 1 << '\n';
-			else cout << 0 << '\n';
-		}
-		else if(proc == 5){
-			if (arr.size() == 0) cout << -1 << '\n';
-			else {
-				cout << arr[arr.size() - 1] << '\n';
-			}
+			s.push(x);
+			break;
+		case 2:
+			cout << s.pop() << '\n';
+			break;
+		case 3:
+			cout << s.size() << '\n';
+			break;
+		case 4:
+			cout << s.empty() << '\n';
+			break;
+		case 5:
+			cout << s.top() << '\n';
 		}
 	}
 	return 0;
