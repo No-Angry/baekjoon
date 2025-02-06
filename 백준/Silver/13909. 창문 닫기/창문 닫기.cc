02840c8bdb20ec26ed33625125f92c50993bc673
SeongHyun Kwon <ashyeon0422@gmail.com>
2025-02-06 11:33:54 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/13909. 창문 닫기/창문 닫기.cc
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -1,17 +1,10 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <set>
-#include <numeric>
 using namespace std;
 
 int main(void) {
 	int x, result = 0;
 	cin >> x;
-	for (int i = 1; i * i <= x; i++) {
-		result += 1;
-	}
+	for (int i = 1; i * i <= x; i++) result += 1;
 	cout << result;
 
 	return 0;
