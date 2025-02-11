e73e73c692b7748c4ccdfcf9dc933060256debd5
SeongHyun Kwon <ashyeon0422@gmail.com>
2025-02-11 16:37:33 +0900

    [Gold II] Title: 돌 게임 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9661. 돌 게임 7/돌 게임 7.cc
+++ 백준/Gold/9661. 돌 게임 7/돌 게임 7.cc
@@ -1,23 +1,10 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <set>
-#include <numeric>
 using namespace std;
 
 int main(void) {
-	long long input, check = 1;
-	int cnt = 0;
+	long long input;
 	cin >> input;
-
-	while (input >= check * 4) {
-		check *= 4;
-		cnt++;
-	}
-	long long m = input - check - 3;
-	vector<int> rule = { 0,1,0,1,0 };
-	if (cnt % 2) rule[0] = 1, rule[1] = 0;
-	cout << (rule[m % 5] ? "CY\n" : "SK\n");
+	int rule[5] = {1,0,1,0,0};
+	cout << (rule[input % 5] ? "CY\n" : "SK\n");
 	return 0;
 }
