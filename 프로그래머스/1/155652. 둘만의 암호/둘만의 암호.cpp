0cffddebb113802f73a8783f18880f67cf51ba93
SeongHyun Kwon <ashyeon0422@gmail.com>
2025-06-09 17:31:08 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
@@ -9,13 +9,7 @@ string solution(string s, string skip, int index) {
         for(int j = 0; j < index; j++){ // index만큼 증가
             s[i] += 1;
             if(s[i] > 'z') s[i] = 'a'; // 증가한 후 z보다 크면 a로 변경
-            for(int l = 0; l < skip.length(); l++){ // skip에 포함된 글자인지 확인
-                if(s[i] == skip[l]){ // skip에 포함되면
-                    s[i] += 1;  // +1
-                    if(s[i] > 'z') s[i] = 'a';
-                    l = -1; // 재검사
-                }
-            }
+            for(int l = 0; l < skip.length(); l++) if(s[i] == skip[l]) j--;
         }
         
     }
