3e43e2ab7284a5bc8d8e7cf823532bfdf8eaa22d
SeongHyun Kwon <ashyeon0422@gmail.com>
2025-06-20 13:22:13 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+    int flag;
+    for(int i = left; i <= right; i++){
+        flag = 0;
+        for(int j = 1; j * j <= i; j++){
+            if(j * j == i){
+                flag = 1;
+            }
+        }
+        answer += (flag ? -i : i); 
+    }
+    return answer;
+}
